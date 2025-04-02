779f03f32d5ae834d1a4b33a75b7d8cae2d711a7
govindsinghmalavat <govind.singh@bjss.com>
2025-04-02 10:51:31 +0000

    data types

---
+++ basic_01/variable_01.js
@@ -0,0 +1,20 @@
+console.log("Govind");
+const accountId = 12345
+let accountEmail = 'xyz.gmail'
+var accountPassword = '123345567'
+accountCity = 'Jaipur'
+let accountState
+
+//accountId = 54321 //not allowed 
+
+accountEmail = "govind.singh@gmail.com"
+accountPassword = "212112112"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
